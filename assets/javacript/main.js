236ccedb02953fb352c4141167395e231a327466
NguyenGiaHuy2k5 <khoahocgiahuy@gmail.com>
2023-11-17 09:24:03 +0700

    first commit

---
+++ assets/javacript/main.js
@@ -0,0 +1,33 @@
+let changeview=document.querySelector("#bg1")
+let i=0;
+changeview.addEventListener("click",function(){
+    if(i==0){
+        document.querySelector(".background_image img").src="./assets/image/bg2.jpg"
+        i++
+    }
+    else if (i==1){
+        document.querySelector(".background_image img").src="./assets/image/bg1.jpg"
+        i++
+    }
+    else {
+        document.querySelector(".background_image img").src="./assets/image/bg3.jpeg"
+        i=0
+    }
+})
+let changesound=document.querySelector("#bg2")
+let j=0;
+changesound.addEventListener("click",function(){
+    if(j==0){
+        document.querySelector(".audios audio").src="./assets/audio/whale1.mp3"
+        j++
+    }
+    else if(j==1){
+        document.querySelector(".audios audio").src="./assets/audio/whale2.mp3"
+        j++
+    }
+    else{
+        document.querySelector(".audios audio").src="./assets/audio/audio1.mp3"
+        j=0
+    }
+    
+})
